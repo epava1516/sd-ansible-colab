05396db210feeba025048ee69514640297bb2a67
Eduardo David Paredes Vara <paredes1516@gmail.com>
2023-04-27 00:16:08 +0200

    mount

--- sd-setup.sh
+++ sd-setup.sh
@@ -4,5 +4,5 @@ cd /tmp
 rm -rf /tmp/sd-ansible-colab
 git clone https://github.com/epava1516/sd-ansible-colab.git
 cd /tmp/sd-ansible-colab
-ansible-playbook main.yml
+ansible-playbook -vvvvv main.yml
 cd /content
